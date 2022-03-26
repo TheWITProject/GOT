f1d8c9287768907b67cc9e1ca98215a859715ae2
Feizza <feizza01@gmail.com>
2022-03-26 01:37:44 -0400

    added grid

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CareerMap from './main_pages/CareerMap';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CareerMap />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/main_pages/CareerMap.js
@@ -0,0 +1,145 @@
+import './CareerMap.css';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+
+
+function CareerMap() {
+    return (
+        <div className="CareerMap">
+            <p>Welcome to Career Map!</p>
+            <Grid container spacing={8}>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <Box
+                        sx={{
+                            width: 250,
+                            height: 250,
+                            marginLeft: 10,
+                            backgroundColor: 'primary.dark',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default CareerMap;
