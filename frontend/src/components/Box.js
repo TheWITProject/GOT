6c9cc7318b67d116be8b5e861236905c9681f7df
Kim2507 <mykim250795@gmail.com>
2022-03-29 14:23:16 -0400

    save changes

--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -1,8 +1,10 @@
+import './CareerMap.css';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Link } from "react-router-dom";
+import CardGrid from '../components/CardGrid.js'
 
 function Box(props) {
   const defaultBoxSX ={
