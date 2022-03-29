c8f9dbf3d65e054ad671a92d0213ef47c032dbee
Feizza <feizza01@gmail.com>
2022-03-29 16:15:09 -0400

    interactive background added

---
+++ frontend/src/components/InteractiveBackground.js
@@ -0,0 +1,100 @@
+
+import Particles from "react-tsparticles";
+
+
+function InteractiveBackground() {
+    const particlesInit = (main) => {
+    console.log(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+            enable: true
+          },
+        background: {
+    
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 1,
+              size: 50,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 50,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#f0c4bb",
+          },
+          links: {
+            color: "#f0c4bb",
+            distance: 150,
+            enable: true,
+            opacity: 1,
+            width: 2,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 1,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
+
+export default InteractiveBackground;
--- frontend/src/main_pages/CareerMap.js
+++ frontend/src/main_pages/CareerMap.js
@@ -1,19 +1,15 @@
 import './CareerMap.css';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { Link } from "react-router-dom";
 import CardGrid from '../components/CardGrid.js'
-
-//property drilling -> passing property card type
-
+import InteractiveBackground from '../components/InteractiveBackground.js';
 
 function CareerMap() {
     return (
         <div>
+            <InteractiveBackground/>
+            <div id = "position">
             <p>Welcome to Career Map!</p>
             <CardGrid cardType = {"careerMap"}/>
+            </div>
         </div>
     );
 }
