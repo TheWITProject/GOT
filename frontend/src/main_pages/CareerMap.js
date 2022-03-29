35bbd9bba54f09e43a0d87de8c84fea6c2cbbdb9
Kim2507 <mykim250795@gmail.com>
2022-03-29 15:01:42 -0400

    save

--- frontend/src/main_pages/CareerMap.js
+++ frontend/src/main_pages/CareerMap.js
@@ -1,5 +1,5 @@
 import './CareerMap.css';
-//import { styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
