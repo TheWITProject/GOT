63816a4eb13419c7643950cac6ee11f01e211d70
Lily <liang1220lily@gmail.com>
2022-04-10 14:23:13 -0400

    trying to run server

--- backend/settings.py
+++ backend/settings.py
@@ -127,5 +127,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
