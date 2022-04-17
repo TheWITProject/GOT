da52032fda0e6ba7c35902e1ed2bd253f841f1e0
Feizza <feizza01@gmail.com>
2022-04-17 15:08:10 -0400

    backend working ish

--- backend/settings.py
+++ backend/settings.py
@@ -133,4 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
-CORS_ALLOW_ALL_ORIGINS: True
+#CORS_ALLOW_ALL_ORIGINS: True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+]
