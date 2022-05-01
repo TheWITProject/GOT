739d054dc29c1af53d31a367c378e92e7198cbf6
Lily <liang1220lily@gmail.com>
2022-05-01 12:55:07 -0400

    saving progress

--- frontend/src/main_pages/CareerMap.js
+++ frontend/src/main_pages/CareerMap.js
@@ -22,10 +22,11 @@ function CareerMap() {
   let getNotes = async () =>{
     let response = await fetch("http://127.0.0.1:8000/api/notes/")
     let data = await response.json()
-    console.log('Data:',data)
+    //console.log('DATA:', data)
     setNotes(data)
   }
   return (
+    
     <div>
       <InteractiveBackground />
 
