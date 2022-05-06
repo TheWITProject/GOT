b1cd7b71085a62787419c0ca80484704c9af55dd
Lily <liang1220lily@gmail.com>
2022-05-05 20:22:35 -0400

    trying to merge login/signup

--- api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Note
-from .models import Careers
-
-admin.site.register(Note)
-admin.site.register(Careers)
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('notes/', views.getNotes, name = "notes"),
-    path('notes/<str:pk>/update/',views.updateNote, name="update-note"),
-    path('notes/<str:pk>/',views.getNote, name="note"),
-    path('careers/', views.getCareers, name="careers")
-]
