490dadb9c8140ba71dc7b3cff37587f4d88be19e
Feizza <feizza01@gmail.com>
2022-05-07 12:19:34 -0400

    trying to merge

--- frontend/src/components/ProfessionalForm/ProfessionalForm.js
+++ frontend/src/components/ProfessionalForm/ProfessionalForm.js
@@ -1,134 +1,3 @@
-// import React, { useRef } from "react";
-// import Box from "@mui/material/Box";
-// import Modal from "@mui/material/Modal";
-// import Fab from "@mui/material/Fab";
-// import FormComponent from "../FormComponent/FormComponent.js";
-// import "./ProfessionalForm.css";
-// import mailbox from "../../assets/graphics/mailbox.gif"
-
-// const styleBox = {
-//   position: "absolute",
-//   top: "50%",
-//   left: "50%",
-//   transform: "translate(-50%, -50%)",
-//   width: "70%",
-//   height: "70%",
-//   bgcolor: "background.paper",
-//   border: "2px solid #000",
-//   boxShadow: 24,
-//   p: 4,
-//   overflow: "scroll",
-// };
-// const styleModal = {
-
-//   height: "100%",
-//   width: "100%"
-// };
-
-// export default function Login() {
-//   const [open, setOpen] = React.useState(false);
-//   const handleOpen = () => setOpen(true);
-//   const handleClose = () => setOpen(false);
-//   const valueRef = useRef(""); //creating a refernce for TextField Component
-
-//   const sendValue = () => {
-//     return console.log(valueRef.current.value); //on clicking button accesing current value of TextField and outputing it to console
-//   };
-
-//   return (
-//     <div>
-//       <Fab
-//         onClick={handleOpen}
-//         sx={{
-//           marginTop: 6,
-//           marginBottom: 2,
-//           fontFamily: "Poppins",
-//           backgroundColor: "#FAE1DD",
-//         }}
-//         variant="extended"
-//         size="medium"
-//       >
-//         Share Your Story
-//       </Fab>
-//       <Modal
-//         open={open}
-//         onClose={handleClose}
-//         aria-labelledby="modal-modal-title"
-//         aria-describedby="modal-modal-description"
-//         sx={styleModal}
-//       >
-//         <Box sx={styleBox}>
-//           <div id="pf_outer">
-//             <h2>
-//               Share your story with &lt;/GOT&gt;!
-//             </h2>
-//             <div id="pf_top">
-//               <div id="pf_left">
-//                 <FormComponent
-//                   title="Hey there, my name is..."
-//                   placeholder="Enter Your Name"
-//                 />
-//                 <FormComponent
-//                   title="I currently am a..."
-//                   placeholder="Enter Job Title @ Company"
-//                 />
-//                 <FormComponent
-//                   title="Checkout my LinkedIn at..."
-//                   placeholder="Enter Your LinkedIn URL Here"
-//                 />
-//                 <FormComponent
-//                   title="My hobbies are..."
-//                   placeholder="Enter your two hobbies"
-//                 />
-//                 <FormComponent
-//                   title="Years of Experience"
-//                   placeholder="Enter Your Name"
-//                 />
-//               </div>
-//               <div id="pf_right">
-//                 <img id ="mailbox"height="500vmin" src={mailbox} alt="mailbox" />
-//               </div>
-//             </div>
-
-//             <div id="pf_bottom">
-//               <h2>Let's move onto the fun part!</h2>
-//               <FormComponent
-//                   title="A little bit about yourself:"
-//                   placeholder=""
-//                 />
-//                 <FormComponent
-//                   title="What do you do as X (position) at X(company)?"
-//                   placeholder=""
-//                 />
-//                 <FormComponent
-//                   title="What made you intered in and pursue X career?"
-//                   placeholder=""
-//                 />
-//                 <FormComponent
-//                   title="How does your company culture align with your own goals and aspirations? "
-//                   placeholder=""
-//                 />
-//                 <FormComponent
-//                   title="Any advice on how to stand out and get hired for those starting off?
-//                   "
-//                   placeholder=""
-//                 />
-//                 <FormComponent
-//                   title="What are some must-have resources or skills you would recommend for your industry?"
-//                   placeholder=""
-//                 />
-//                 <FormComponent
-//                   title="What was your big down time ( failure) in jobs applying/interviews etc and how did you go through that?"
-//                   placeholder=""
-//                 />
-//             </div>
-//           </div>
-//         </Box>
-//       </Modal>
-//     </div>
-//   );
-// }
-
 import React, { useReducer } from "react";
 import { Button, Icon, TextField, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -212,16 +81,16 @@ export default function ProfessionalForm(props) {
     let data = { formInput };
     console.log(JSON.stringify(data));
     handleClose();
-    // fetch("https://pointy-gauge.glitch.me/api/form", {
-    //   method: "POST",
-    //   body: JSON.stringify(data),
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // })
-    //   .then(response => response.json())
-    //   .then(response => console.log("Success:", JSON.stringify(response)))
-    //   .catch(error => console.error("Error:", error));
+    fetch("/api/stories/create", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(response => console.log("Success:", JSON.stringify(response)))
+      .catch(error => console.error("Error:", error));
   };
 
   const handleInput = (evt) => {
@@ -386,42 +255,6 @@ export default function ProfessionalForm(props) {
         </Box>
       </Modal>
 
-      {/*       
-      <Paper className={classes.root}>
-        {/* <Typography variant="h5" component="h3">
-          {props.formName}
-        </Typography>
-        <Typography component="p">{props.formDescription}</Typography> */}
-
-      {/* <form onSubmit={handleSubmit}>
-          <TextField
-            label="Name"
-            id="margin-normal"
-            name="name"
-            defaultValue={formInput.email}
-            className={classes.textField}
-            helperText="Enter your full name"
-            onChange={handleInput}
-          />
-          <TextField
-            label="Email"
-            id="margin-normal"
-            name="email"
-            defaultValue={formInput.name}
-            className={classes.textField}
-            helperText="e.g. name@gmail.com"
-            onChange={handleInput}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            Subscribe <Icon className={classes.rightIcon}>send</Icon>
-          </Button>
-        </form>
-      </Paper> */}
     </div>
   );
 }
