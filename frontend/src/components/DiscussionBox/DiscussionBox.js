7fb07f07769971c84c74c580eb65badbe7c91fbd
Kim2507 <mykim250795@gmail.com>
2022-05-07 15:42:39 -0400

    save changes
    Co-authored-by: Feizza Fazilatun <feizza615@users.noreply.github.com>

--- frontend/src/components/DiscussionBox/DiscussionBox.js
+++ frontend/src/components/DiscussionBox/DiscussionBox.js
@@ -1,16 +1,42 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import SearchBar from '../SearchBar/SearchBar';
+import SelectBox from '../SelectBox/SelectBox';
 
 
 export default function BoxSx() {
     return (
-      <><Box
+      <div>
+      <div>
+      <SearchBar/>
+      <Button color="grey">Create Post</Button>
+      </div>
+      <SelectBox  label = {"User Type"}
+                  array ={["1","2","3"]}
+                  
+                  />
+
+      <SelectBox  label = {"Most Popular"}
+                  array ={["Most Rated","N/A","N/A"]}
+                  
+                        />
+      
+      <SelectBox label = {"Most Relevant"}
+                  array ={["N/A","N/A","N/A"]}
+                  />
+      
+      <SelectBox label = {"Topic"}
+                  array ={["SE","UI/UX","N/A"]}
+                  />
+      
+            
+      <Box
             sx={{
                 p :5,
                 marginRight: "10px",
                 marginLeft: "10px",
-                width: 1200,
+                width: "fix",
                 height: 300,
                 color: "red",
                 backgroundColor: 'yellow',
@@ -23,23 +49,26 @@ export default function BoxSx() {
                     p: 5,
                     marginRight: "10px",
                     marginLeft: "10px",
-                    width: 1200,
+                    width: "fix",
                     height: 300,
                     backgroundColor: 'primary.dark',
                     
-                }} /><Box
+                }} />
+                
+                <Box
                 sx={{
                     m: 5,
                     p: 5,
                     marginRight: "10px",
                     marginLeft: "10px",
-                    width: 1200,
+                    width: "fix",
                     height: 300,
                     color: "red",
                     backgroundColor: 'yellow',
                     
-                }} /></>
-        
+                }} />
+                
+                </div>
     );
   }
 
---
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "fit"
+  }
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(2, 2, 2, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "fit",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch"
+      }
+    }
+  }
+}));
+
+export default function SearchAppBar() {
+  return (
+  
+    <AppBar position={"static"}
+            color ={"inherit"}
+            >
+      <Toolbar>
+        <Search>
+          <StyledInputBase
+            placeholder="Search for a post"
+            inputProps={{ "aria-label": "search" }}
+          />
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+        </Search>
+        </Toolbar>
+ 
+      </AppBar>
+
+  );
+}
